ac3f847e610c31b88a4bf34a959ffb08c64296f0
Mahmoud <mahmoud@eramint.com>
2020-06-15 09:29:54 +0200

    edit sign up

--- resources/templates/template-singup.blade.php
+++ resources/templates/template-singup.blade.php
@@ -8,48 +8,93 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
 
-    <div class="custom-header">
-    <div class="elementor-background-overlay" style="background-image:url('{{ the_field('header_section_image', 'option') }}')"></div>
-      @include('partials.page-header')
-    </div>
-
-    <div class="container-fluid">
-      <div class="row align-items-center">
-    
-        <div class="col-md-5 col-12 hv-100" style="background-image: linear-gradient(150deg, #56ecf2 0%, #4242e3 100%);">
-          <div class="elementor-background-overlay" style="background-image: url('https://premast.com/app/uploads/2019/04/16952-Converted-01.png');"></div>
-          <div class="col-12 description">
-            <h4 class="text-white title-description">Welcome to premast</h4>
-            <p class="text-white text-description mb-3">Join us and enjoy with this benefits</p> 
-            <p class="text-white min-description">* Recieve a 20% off discount in your E-mail</p>
-            <p class="text-white min-description">* Downloads hunderds of powerpoint slides and graphics for free</p>
-            <p class="text-white min-description">* Discover amazing new products daily</p>
+  @if ( is_user_logged_in() ) 
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-md-6 col-12 modal-forms">
+          <div id="formBlock">
+            <img src="{{ get_theme_file_uri().'/resources/assets/images' }}/thanks.png" alt="icon thanks">
+            <h3>Your account has been registered!</h3>
+            <p>Thanks for using premast plus. <br> Now you can access our plugin anytime</p>
+            <a class="link-app" href="<?= the_field('link_app_install', 'option'); ?>">
+              <img src="<?= the_field('img_app_install', 'option'); ?>" alt="install app">
+            </a>
           </div>
         </div>
-
-        <div class="col-md-7 col-12 hv-100 sign-up">
-          <a class="navbar-brand" href="https://premast.com/" title="Premast">
-            <img class="img-fluid" src=" https://premast.com/app/uploads/2019/04/1-copy-4.png " alt="Premast" title="Premast">
-            <span class="sr-only"> Premast </span>
-          </a>
-          <br>
-          <h5 class="modal-title" id="SignupUserLabel">Create a New Premast Account</h5>
-          @include('partials.content-page')
-        </div>
-        
       </div>
     </div>
+  @else 
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-md-6 col-12 modal-forms">
+          <div id="formBlock">
+            <h3>Create New Account</h3>
+            <p>Make stunning PowerPoint designs faster than ever.</p>
+            @include('partials.content-page')
+          </div>
+        </div>
+      </div>
+    </div>
+  @endif
 
-    <style>
-      .hv-100 {
-        height: 100vh;
-        padding: 100px 15px;
-        min-height: 600px;
-      }
-      .form-group.submit-button button {
-        padding: 10px 20px;
-        color: #fff;
-      }      
-    </style>
+
+  <style>
+    .modal-forms {
+      height: 100vh;
+      min-height: 600px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .modal-forms h2 {
+      display: none;
+    }
+
+    .modal-forms label {
+      display: none;
+    }
+
+    .modal-forms #formBlock {
+      background: #FFFFFF;
+      box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.2);
+      border-radius: 4px;
+      width: 100%;
+      padding: 30px;
+      text-align: center;
+    }
+
+    div#reg_forms {
+      width: 100%;
+    }
+
+    .modal-forms #formBlock input {
+        background: #F9F9F9;
+        border: 1px solid #BEC6D2;
+        box-sizing: border-box;
+        border-radius: 4px;
+        height: 40px;
+    }
+
+    .modal-forms #formBlock input[type="submit"] {
+      background: #1E6DFB;
+      border-radius: 4px;
+      color: #fff;
+      width: 100% !important;
+      margin: 0 !important;
+      height: 40px;
+    }
+
+    div#formBlock h3 {
+      font-family: Roboto;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 30px;
+      line-height: 36px;
+      color: #000000;
+      margin: 30px 0;
+    }
+  </style>
+
   @endwhile
 @endsection

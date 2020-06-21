1684eea385cca8af6d8408f0d330d415bdb9ddb4
Mahmoud <mahmoud@eramint.com>
2020-06-21 11:27:25 +0200

    Add update & delete

--- framework/api/add-post.php
+++ framework/api/add-post.php
@@ -1,4 +1,6 @@
 <?php
+
+// API POST
 function action_posts($data){
 
   $data=$data->get_params('POST');
@@ -11,6 +13,7 @@ function action_posts($data){
   $file = !empty($file) ? $file : false;
   $term = !empty($term) ? $term : false;
   $tag = !empty($tag) ? $tag : false;
+  $author = !empty($author) ? $author : false;
 
   $args = array(
     'count_total'  => false,
@@ -30,6 +33,7 @@ function action_posts($data){
     }
 
     $user = get_user_by( 'email', $email );
+    $author_id = get_user_by( 'email', $author );
 
     if (in_array($email, $emails) && wp_check_password( $password, $user->data->user_pass, $user->ID)) { 
       
@@ -37,7 +41,7 @@ function action_posts($data){
         'post_type' => 'graphics',
         'post_title' => $title,
         'post_status' => 'publish',
-        'post_author' => $user->ID,
+        'post_author' => $author_id->ID,
         'tax_input' => array( 'graphics-category' => array($term), 'graphics-tag' => $tag)
       ));
 
@@ -154,6 +158,265 @@ add_action('rest_api_init' , function(){
           return true;
         }
       ),   
+      'author' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ), 
+    )
+  ));
+});
+
+
+// API UPDATE
+function get_api_posts($data){
+
+  $data=$data->get_params('GET');
+  extract($data);
+
+  $email = !empty($email) ? $email : false;
+  $password = !empty($password) ? $password : false;
+  $title = !empty($title) ? $title : "test";
+  $image = !empty($image) ? $image : false;
+  $file = !empty($file) ? $file : false;
+  $term = !empty($term) ? $term : false;
+  $tag = !empty($tag) ? $tag : false;
+  $author = !empty($author) ? $author : false;
+  $post_id = !empty($post_id) ? $post_id : false;
+
+  $args = array(
+    'count_total'  => false,
+    'fields'       => 'all',
+  ); 
+  
+  $users = get_users( $args );
+
+  if($email && $password) {
+
+    $emails = [];
+    $passwords = [];
+
+    foreach ($users as $user) {
+      $passwords[] = $user->user_pass;
+      $emails[] =  $user->user_email;
+    }
+
+    $user = get_user_by( 'email', $email );
+    $author_id = get_user_by( 'email', $author );
+
+    if (in_array($email, $emails) && wp_check_password( $password, $user->data->user_pass, $user->ID)) { 
+      
+
+      $graphics = wp_update_post(array (
+        'post_type' => 'graphics',
+        'ID'           => $post_id,
+        'post_title' => $title,
+        'post_status' => 'publish',
+        'post_author' => $author_id->ID,
+        'tax_input' => array('graphics-category' => $term, 'graphics-tag' => $tag)
+      ));
+
+
+      if ($graphics) {
+        if($file) {
+          $file_id = Generate_Featured_Image($file);
+          update_field( 'field_5d43723a031b2', $file_id, $graphics );
+        }
+        
+        if($image) {
+          $attach_id = Generate_Featured_Image($image);
+          set_post_thumbnail( $graphics, $attach_id );
+        }
+      }
+
+
+        
+
+      $args = array(
+        'post_type'   => 'graphics',
+        'post_status' => 'publish',
+        'post__in'    => array($graphics)
+      );
+
+      $posts = get_posts($args);
+
+      if (!is_wp_error($posts)) {
+        $message = 'successfully Login';
+      }
+      
+      add_action('acf/save_post', 'my_acf_save_post');
+      function my_acf_save_post( $posts ) {
+        wp_update_post($graphics);
+      }
+
+
+      foreach( $posts as &$post ):
+        $terms =  wp_get_post_terms($post->ID , 'graphics-category');
+        if(!empty($terms)){
+          $post->Category= $terms[0]->name;
+        }
+        
+        $file = get_field('file_graphics' , $post->ID);
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+
+        $post->Type = $ext;
+        $post->Id           = $post->ID;
+        $post->Name         = htmlspecialchars_decode( get_the_title($post->ID) );
+        $post->Content = get_field('file_graphics' , $post->ID);
+        $post->PreviewImage = get_the_post_thumbnail_url($post->ID, 'full' );
+        unset($post->ID, $post->post_name, $post->post_type, $post->post_excerpt);
+        formatPost($post);
+      endforeach;
+      
+    } else { 
+      $message = 'Try to login again (login or password) error';
+    } 
+    $result = [
+      'success' => true,
+      'code' => 200,
+      'message' => $message,
+      "data" => $posts,
+    ];
+    return $result;
+  } else {
+    $result = [
+      'success' => false,
+      'code' => 404,
+      'message' => 'login or password not fund',
+    ];
+    return $result;
+  }
+}
+
+add_action('rest_api_init' , function(){
+  register_rest_route('wp/api/' ,'post/add_update',array(
+    'methods' => 'GET',
+    'callback' => 'get_api_posts',
+    'args' => array(
+      'email' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),
+      'password' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),  
+      'title' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),
+      'image' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),   
+      'file' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),   
+      'term' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),  
+      'tag' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),   
+      'author' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ), 
+      'post_id' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ), 
+    )
+  ));
+});
+
+
+// API DELETE
+function delete_api_posts($data){
+
+  $data=$data->get_params('GET');
+  extract($data);
+
+  $email = !empty($email) ? $email : false;
+  $password = !empty($password) ? $password : false;
+  $post_id = !empty($post_id) ? $post_id : "post_id";
+
+
+  $args = array(
+    'count_total'  => false,
+    'fields'       => 'all',
+  ); 
+  
+  $users = get_users( $args );
+
+  if($email && $password) {
+
+    $emails = [];
+    $passwords = [];
+
+    foreach ($users as $user) {
+      $passwords[] = $user->user_pass;
+      $emails[] =  $user->user_email;
+    }
+
+    $user = get_user_by( 'email', $email );
+
+    if (in_array($email, $emails) && wp_check_password( $password, $user->data->user_pass, $user->ID)) { 
+     
+      wp_delete_post($post_id);
+      $message = 'delete item success';
+      
+    } else { 
+      $message = 'Try to login again (login or password) error';
+    } 
+    $result = [
+      'success' => true,
+      'code' => 200,
+      'message' => $message,
+    ];
+    return $result;
+  } else {
+    $result = [
+      'success' => false,
+      'code' => 404,
+      'message' => 'login or password not fund',
+    ];
+    return $result;
+  }
+}
+
+add_action('rest_api_init' , function(){
+  register_rest_route('wp/api/' ,'post/add_delete',array(
+    'methods' => 'GET',
+    'callback' => 'delete_api_posts',
+    'args' => array(
+      'email' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),
+      'password' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),  
+      'post_id' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),
     )
   ));
 });

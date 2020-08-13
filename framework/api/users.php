317e830baaed8d9e2dabacb9c7c3f4fe219d66e7
Mahmoud <mahmoud@eramint.com>
2020-08-13 14:11:33 +0200

    edit api and button in header

--- framework/api/users.php
+++ framework/api/users.php
@@ -1,7 +1,7 @@
 <?php
 function all_users($data){
 
-  $data=$data->get_params('GET');
+  $data=$data->get_params('POST');
   extract($data);
 
   $email = !empty($email) ? $email : false;
@@ -71,7 +71,7 @@ function all_users($data){
 
 add_action('rest_api_init' , function(){
   register_rest_route('wp/api/' ,'users/login',array(
-    'methods' => 'GET',
+    'methods' => 'POST',
     'callback' => 'all_users',
     'args' => array(
       'email' => array(

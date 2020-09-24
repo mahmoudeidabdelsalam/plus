544f36bace3ce8f90a61553f51f1c062091b447e
Mahmoud <mahmoud@eramint.com>
2020-09-24 11:20:51 +0200

    edit log search

--- framework/api/log.php
+++ framework/api/log.php
@@ -429,7 +429,7 @@ function get_log_search($data){
 
 
 
-  if($keyword) {
+  if($keyword && empty($category)) {
     $args = array(
       'post_type'         => 'log_search',
       'post_status'       => 'publish',
